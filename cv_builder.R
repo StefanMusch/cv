bf976c75cf4ce937757503d6c36387e9f88c52be
StefanMusch <stefan_musch2@hotmail.com>
2021-04-27 22:12:56 +0200

    Initial CV Commit - need to edit CSS  & Values

---
+++ cv_builder.R
@@ -0,0 +1,8 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Stefan Musch",
+  data_location = "https://docs.google.com/spreadsheets/d/1YjN_E7Rkw7Nh5Vdeoh0H2b0qSU6WKzBL8yYtWqDwgMQ",
+  pdf_location = "https://github.com/StefanMusch/cv/raw/main/stefan_musch_cv.pdf",
+  html_location = "nickstrayer.me/cv/",
+  source_location = "https://github.com/StefanMusch/cv",
+  use_network_logo = TRUE
+)
